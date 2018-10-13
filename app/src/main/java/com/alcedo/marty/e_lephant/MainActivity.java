33229f5f40a56f826432e3dd49d0bdb9c0e9fe51
Martin CORNU <martin.cornu43@gmail.com>
2018-10-13 22:36:35 +0200

    search bar design ok

--- app/src/main/java/com/alcedo/marty/e_lephant/MainActivity.java
+++ app/src/main/java/com/alcedo/marty/e_lephant/MainActivity.java
@@ -2,12 +2,27 @@ package com.alcedo.marty.e_lephant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+    {
+
+    private List<String> lastSearches;
+    private MaterialSearchBar searchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchBar = (MaterialSearchBar) findViewById(R.id.searchBar);
+        searchBar.setHint("Custom hint");
+
+
     }
 }
