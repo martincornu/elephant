bc8c4210bbc5f73d3522d9a1ffee2201e63bf028
Martin CORNU <martin.cornu43@gmail.com>
2018-10-14 08:23:38 +0200

    live full screen

--- app/src/main/java/com/alcedo/marty/e_lephant/activities/LiveActivity.java
+++ app/src/main/java/com/alcedo/marty/e_lephant/activities/LiveActivity.java
@@ -1,7 +1,11 @@
 package com.alcedo.marty.e_lephant.activities;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.alcedo.marty.e_lephant.R;
 
@@ -11,5 +15,14 @@ public class LiveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live);
+
+        VideoView videoView = findViewById(R.id.live_video_view);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video_tuto;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
 }
