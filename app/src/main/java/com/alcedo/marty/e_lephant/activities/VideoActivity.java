ee8c985e43480ad4002fbe1c11ecc590c2d245c6
Martin CORNU <martin.cornu43@gmail.com>
2018-10-14 04:10:29 +0200

    video view set up

--- app/src/main/java/com/alcedo/marty/e_lephant/activities/VideoActivity.java
+++ app/src/main/java/com/alcedo/marty/e_lephant/activities/VideoActivity.java
@@ -1,7 +1,10 @@
 package com.alcedo.marty.e_lephant.activities;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.alcedo.marty.e_lephant.R;
 
@@ -11,5 +14,14 @@ public class VideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        VideoView videoView = findViewById(R.id.video_view);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video_tuto;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
 }
